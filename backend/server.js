4b7f252d0f7ccd91b9e64f75cb792a6e192b706c
Kinga Kohari <kinguca@yahoo.com>
2022-04-06 16:33:46 +0200

    Images do not show up

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const app = express();
+app.use(express.json());
+
+const port = 9000;
+
+const pathToFrontend = (path.join(`${__dirname}/../frontend`))
+
+app.get("/", (req, res, next) => {                  
+    res.sendFile(`${pathToFrontend}/index.html`);
+}) 
+
+app.get("/image-list", (req, res, next) => {                  
+    res.sendFile(`${pathToFrontend}/data.json`);
+}) 
+
+
+app.use('/pub', express.static(`${pathToFrontend}/pub`));
+
+app.listen(port, () => {
+    console.log(`http://127.0.0.1:${port}`);
+})
---
+++ frontend/pub/script.js
@@ -0,0 +1,40 @@
+const parseJSON = async (url) => {
+    const response = await fetch (url)
+    return response.json()
+}
+
+const swiperComponent = (data, component) => {
+    return `    
+    <div class="swiper">
+        <div class="swiper-wrapper"> 
+            ${data.map(image => component(image)).join("")}
+        </div>
+    </div>
+    `
+}
+
+const swiperSlideComponent = ({filename, title}) => {
+    return `
+    <div class="swiper-slide">
+        <h2>${title}</h2>
+        <img src="./img/${filename}">
+    </div>
+    `
+}
+
+const  loadEvent = async () => {
+
+    const rootElement = document.getElementById("root")
+    const result = await parseJSON("/image-list")
+    
+    rootElement.insertAdjacentHTML("beforeend", swiperComponent(result, swiperSlideComponent))
+
+    const swiper = new Swiper(".swiper", {
+        loop: true
+    })
+
+    console.log(swiper);
+
+}
+
+window.addEventListener("load", loadEvent)
