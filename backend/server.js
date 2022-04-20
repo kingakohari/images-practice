3d5d56ad825cc139fd88d80af44b6d520cd4c9ce
Kinga Kohari <kinguca@yahoo.com>
2022-04-20 21:25:42 +0200

    Partial success

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,10 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const app = express();
+const fileUpload = require('express-fileupload');
 app.use(express.json());
+
+app.use(fileUpload());
 
 const port = 9000;
 
@@ -16,6 +19,50 @@ app.get("/image-list", (req, res, next) => {
     res.sendFile(`${pathToFrontend}/data.json`);
 }) 
 
+
+const uploads = path.join(`${pathToFrontend}/img`)
+
+// If there is a data.json, read the data from the file, if not, use an empty Array
+let jsonData = [];
+try {
+    let data = fs.readFileSync(`${pathToFrontend}/data.json`, error => {
+        if (error) {
+            console.log(error);
+        }
+    });
+    jsonData = JSON.parse(data);
+} catch (error) {
+    fs.writeFile(`${pathToFrontend}/data.json`, JSON.stringify(jsonData), (error) => {
+        if (error) {
+            console.log(error);
+        }
+    });
+}
+
+app.post("/", (req, res) => {
+    // Upload image
+    const picture = req.files.picture;
+    const answer = {};
+
+    if (picture) {
+        picture.mv(uploads + picture.name, error => {
+            return res.status(500).send(error);
+        });
+    }
+    answer.pictureName = picture.name;
+
+    // Upload data from form
+    const formData = req.body;
+    /* formData.image_name = picture.name; */
+    jsonData.push(formData);
+
+    fs.writeFile(`${pathToFrontend}/data.json`, JSON.stringify(jsonData), (error) => {
+        if (error) {
+            console.log(error);
+        }
+    });
+    res.send(answer);
+});
 
 app.use('/pub', express.static(`${pathToFrontend}/pub`));
 app.use('/img', express.static(`${pathToFrontend}/img`));
--- frontend/pub/script.js
+++ frontend/pub/script.js
@@ -13,13 +13,13 @@ const swiperComponent = (data, component) => {
     `
 }
 
-const swiperSlideComponent = ({filename, title, link, photographer}) => {
+const swiperSlideComponent = ({title, link, photographer}) => {
     return `
     <div class="swiper-slide">
         <h2>${title}</h2>
         <h3>Photographer: ${photographer}</h3>
         <p>Source:<br> ${link}</p>
-        <img src="${link}">
+        <img src="img/${res.pictureName}">
         <button id="deleteBtn">Delete this entry</button>
     </div>
     `
@@ -29,10 +29,10 @@ const formComponent = `
     <div class="addNew">
         <h1>Add new image</h1>
         <form id="form">
-            <input type="text" name="title" placeholder="Title" required>
-            <input type="text" name="link" placeholder="URL (if not on hard drive)">
-            <input type="text" name="photographer" placeholder="Photographer's name" required>
-            <input type="file" name="picture">
+            <input type="text" id="title" name="title" placeholder="Title" required>
+            <input type="text" id="link" name="link" placeholder="URL required">
+            <input type="text" id="photographer" name="photographer" placeholder="Photographer's name" required>
+            <input type="file" id="picture" name="picture" required>
             <button id="addBtn">Submit image</button>
         </form>
     </div>
@@ -44,15 +44,49 @@ const  loadEvent = async () => {
     const result = await parseJSON("/image-list")
     
     rootElement.insertAdjacentHTML("afterbegin",formComponent)
-
-    rootElement.insertAdjacentHTML("beforeend", swiperComponent(result, swiperSlideComponent))
+  
 
     const swiper = new Swiper(".swiper", {
         loop: true
     })
 
-    console.log(swiper);
+    const sendForm = document.getElementById("form");
 
+    sendForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const today = new Date();
+        const todayDate = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+
+        const formData = new FormData();
+
+        formData.append("link", document.getElementById("link").value);
+        formData.append("title", document.getElementById("title").value);
+        formData.append("upload_date", todayDate); 
+        formData.append("photographer", document.getElementById("photographer").value);
+        formData.append("picture", document.getElementById("picture").files[0]);
+         
+        const fetchSettings = {
+            method: "POST",
+            body: formData
+        }
+
+        fetch("/", fetchSettings)
+            .then(async data => {
+                if (data.status === 200){
+                    const res = await data.json()
+                    rootElement.insertAdjacentHTML("beforeend", swiperComponent(result, swiperSlideComponent())
+                    console.dir(data)
+                    res.send({response: "Image gallery has been updated!"})
+                }
+            })
+            .catch(error => {
+                e.target.outerHTML= `Error`
+                console.dir(error);
+            })
+    })
+
+  
 }
 
 window.addEventListener("load", loadEvent)
