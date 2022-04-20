a2b19da5a066ee76ab02a30542fd29584c1cd877
Kinga Kohari <kinguca@yahoo.com>
2022-04-20 11:36:14 +0200

    Upload form added, CSS done

--- frontend/pub/script.js
+++ frontend/pub/script.js
@@ -20,15 +20,31 @@ const swiperSlideComponent = ({filename, title, link, photographer}) => {
         <h3>Photographer: ${photographer}</h3>
         <p>Source:<br> ${link}</p>
         <img src="${link}">
+        <button id="deleteBtn">Delete this entry</button>
     </div>
     `
 }
+
+const formComponent = `
+    <div class="addNew">
+        <h1>Add new image</h1>
+        <form id="form">
+            <input type="text" name="title" placeholder="Title" required>
+            <input type="text" name="link" placeholder="URL (if not on hard drive)">
+            <input type="text" name="photographer" placeholder="Photographer's name" required>
+            <input type="file" name="picture">
+            <button id="addBtn">Submit image</button>
+        </form>
+    </div>
+`;
 
 const  loadEvent = async () => {
 
     const rootElement = document.getElementById("root")
     const result = await parseJSON("/image-list")
     
+    rootElement.insertAdjacentHTML("afterbegin",formComponent)
+
     rootElement.insertAdjacentHTML("beforeend", swiperComponent(result, swiperSlideComponent))
 
     const swiper = new Swiper(".swiper", {
