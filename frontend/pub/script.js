783887bc078ccba688378f691d6f58db8f6b9deb
Adam Elek <adam.elek@ext.codecool.com>
2022-04-29 16:53:13 +0200

    somewhat working version

--- frontend/pub/script.js
+++ frontend/pub/script.js
@@ -1,30 +1,29 @@
 const parseJSON = async (url) => {
-    const response = await fetch (url)
-    return response.json()
-}
+	const response = await fetch(url);
+	return response.json();
+};
 
 const swiperComponent = (data, component) => {
-    return `    
+	return `    
     <div class="swiper">
         <div class="swiper-wrapper"> 
-            ${data.map(photoInfo => component(photoInfo)).join("")}
+            ${data.map((photoInfo) => component(photoInfo)).join("")}
         </div> 
     </div>
-    `
-    
-}
+    `;
+};
 
-const swiperSlideComponent = ({title, link, photographer}) => {
-    return `
-    <div class="swiper-slide">
+const swiperSlideComponent = ({ id, title, link, photographer }) => {
+	return `
+    <div class="swiper-slide" id=${id}>
         <h2 class="title">${title}</h2>
         <h3>Photographer: ${photographer}</h3>
         <p>Source:<br> ${link}</p>
         <img src="${link}">      
         <button class="deleteBtn">Delete this entry</button>
     </div>
-    `
-}
+    `;
+};
 
 const formComponent = `
     <div class="addNew">
@@ -39,95 +38,92 @@ const formComponent = `
     </div>
 `;
 
-const  loadEvent = async () => {
+const loadEvent = async () => {
+	const rootElement = document.getElementById("root");
+	const result = await parseJSON("/image-list");
 
-    const rootElement = document.getElementById("root")
-    const result = await parseJSON("/image-list")
-    
-    rootElement.insertAdjacentHTML("afterbegin", formComponent)
+	rootElement.insertAdjacentHTML("afterbegin", formComponent);
 
-    rootElement.insertAdjacentHTML("beforeend", swiperComponent(result, swiperSlideComponent))
-  
+	rootElement.insertAdjacentHTML(
+		"beforeend",
+		swiperComponent(result, swiperSlideComponent)
+	);
 
-    const swiper = new Swiper(".swiper", {
-        loop: true
-    })
+	const swiper = new Swiper(".swiper", {
+		loop: true,
+	});
 
-    const sendForm = document.getElementById("form");
+	const sendForm = document.getElementById("form");
 
-    sendForm.addEventListener("submit", (e) => {
-        e.preventDefault();
+	sendForm.addEventListener("submit", (e) => {
+		e.preventDefault();
 
-        const today = new Date();
-        const todayDate = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+		const today = new Date();
+		const todayDate =
+			today.getFullYear() +
+			"-" +
+			(today.getMonth() + 1) +
+			"-" +
+			today.getDate();
 
-        const formData = new FormData();
+		const formData = new FormData();
 
-        formData.append("link", document.getElementById("link").value);
-        formData.append("title", document.getElementById("title").value);
-        formData.append("upload_date", todayDate); 
-        formData.append("photographer", document.getElementById("photographer").value);
-        formData.append("picture", document.getElementById("picture").files[0]);
-        formData.append("id");
-         
-        const fetchSettings = {
-            method: "POST",
-            body: formData
-        }
+		formData.append("link", document.getElementById("link").value);
+		formData.append("title", document.getElementById("title").value);
+		formData.append("upload_date", todayDate);
+		formData.append(
+			"photographer",
+			document.getElementById("photographer").value
+		);
+		formData.append("picture", document.getElementById("picture").files[0]);
 
-        fetch("/", fetchSettings)
-            .then(async data => {
-                if (data.status === 200){
-                    const res = await data.json()
-                    console.log(res);
+		const fetchSettings = {
+			method: "POST",
+			body: formData,
+		};
 
-                    const title = res.title
-                    const link = res.link
-                    const photographer = res.photographer
-                        
+		fetch("/", fetchSettings)
+			.then(async (data) => {
+				if (data.status === 200) {
+					const res = await data.json();
+					console.log(res);
 
-                    /* rootElement.insertAdjacentHTML("beforeend", swiperComponent(result, swiperSlideComponent(res))) */
-                    swiper.appendSlide((result, swiperSlideComponent({title, link, photographer}))) 
-                    swiper.update(); 
-                    console.dir(data)
-                    /* res.send({response: "Image gallery has been updated!"}) */
-                    
-                }
-            })
-            .catch(error => {
-                e.target.outerHTML= `Error`
-                console.dir(error);
-            })
-    })
+					const id = res.id;
+					const title = res.title;
+					const link = res.link;
+					const photographer = res.photographer;
 
-    const removeButtons = document.getElementsByClassName("deleteBtn"); 
-    
-    Array.from(removeButtons).forEach((removeButton) => {
-        removeButton.addEventListener('click', () => {
-          removeButton.parentNode.remove();
-          let id = removeButton.parentNode.getAttribute("data-swiper-slide-index")
-          let title = {title: removeButton.parentNode.getElementsByClassName("title")[0].innerHTML}
-          console.log(title);
-          fetch('/delete/' + id , {
-            method: 'DELETE',
-            header: {'Content-Type': 'application/json'},
-            body: JSON.stringify(title)
-          })
-          .then(res => res.text()) // or res.json()
-          .then(res => console.log(res))
-        });
+					swiper.appendSlide(
+						(result, swiperSlideComponent({ id, title, link, photographer }))
+					);
+					addEventListenersToDeleteButtons();
+					swiper.update();
+				}
+			})
+			.catch((error) => {
+				e.target.outerHTML = `Error`;
+				console.dir(error);
+			});
+	});
 
-      });
+	const addEventListenersToDeleteButtons = () => {
+		const removeButtons = document.getElementsByClassName("deleteBtn");
 
-/*      swiper.removeSlide(swiper.realIndex)
-        console.log(swiper.realIndex);
-        swiper.update() */
-      
-        
-       
+		Array.from(removeButtons).forEach((removeButton) => {
+			removeButton.addEventListener("click", () => {
+				let id = removeButton.parentNode.id;
+				fetch("/delete/" + id, {
+					method: "DELETE",
+				})
+					.then((res) => res.text()) // or res.json()
+					.then((res) => console.log(res));
 
-       
-  
-}
+				swiper.removeSlide(swiper.realIndex);
+				swiper.update();
+			});
+		});
+	};
+	addEventListenersToDeleteButtons();
+};
 
-window.addEventListener("load", loadEvent)
+window.addEventListener("load", loadEvent);
